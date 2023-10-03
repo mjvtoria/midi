d2dd534f0d06e6abc076149f74c8efbfeb9c5029
Marjory <105371524+mjvtoria@users.noreply.github.com>
2023-10-03 07:56:34 -0300

    .

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ alert('Olá, mundo');
 
 function tocaSomPom(){
     document.querySelector('.tecla_pom').play ;
-    document.querySelector('.tecla_pom').onclick = tocaSomPom; ;
+    document.querySelector('.tecla_pom').onclick=tocaSomPom; ;
 }
