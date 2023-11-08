e27864efd901c34633e63c85836625769fcbd8ec
Marjory <105371524+mjvtoria@users.noreply.github.com>
2023-11-08 11:10:46 -0300

    .

--- main.js
+++ main.js
@@ -1,48 +1,22 @@
 alert('Olá, mundo');
 
-function tocaSomPom(){
-    document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSom;
+
+for(let contador = 0;contador < listaDeTeclas.length;contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
     }
-    document.querySelector(".tecla_pom").onclick = tocaSomPom;
-
-function tocaSomClap(){
-    document.querySelector("#som_tecla_clap").play();
-    }
-    document.querySelector(".tecla_clap").onclick = tocaSomClap;
-
-    function tocaSomTim(){
-        document.querySelector("#som_tecla_tim").play();
-        }
-        document.querySelector(".tecla_tim").onclick = tocaSomTim;
-
-        function tocaSomFuff(){
-            document.querySelector("#som_tecla_puff").play();
-            }
-            document.querySelector(".tecla_puff").onclick = tocaSomPuff;
-
-            function tocaSomSplash(){
-                document.querySelector("#som_tecla_splash").play();
-                }
-                document.querySelector(".tecla_splash").onclick = tocaSomSplash;
-
-                function tocaSomToim(){
-                    document.querySelector("#som_tecla_toim").play();
-                    }
-                    document.querySelector(".tecla_toim").onclick = tocaSomToim; 
-
-                    function tocaSomPsh(){
-                        document.querySelector("#som_tecla_psh").play();
-                        }
-                        document.querySelector(".tecla_psh").onclick = tocaSomPsh;
-
-                        function tocaSomTic(){
-                            document.querySelector("#som_tecla_tic").play();
-                            }
-                            document.querySelector(".tecla_tic").onclick = tocaSomTic;
-
-                            function tocaSomTom(){
-                                document.querySelector("#som_tecla_tom").play();
-                                }
-                                document.querySelector(".tecla_tom").onclick = tocaSomTom;
-
-                                document.querySelectorAll(".tecla");
+    
+   
+}
